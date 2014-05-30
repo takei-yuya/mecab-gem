c22cd5e88178caf13d3fba6f5693f85c5747629a
Mark Burns <markthedeveloper@gmail.com>
2014-05-30 16:58:54 +0900

    add LDFLAGS as per http://insideflag.blogspot.jp/2012/09/mecab-rubymac.html

--- ext/mecab/extconf.rb
+++ ext/mecab/extconf.rb
@@ -9,6 +9,8 @@ find_executable('make')
 mecab_config = with_config('mecab-config', 'mecab-config')
 use_mecab_config = enable_config('mecab-config')
 
+$LDFLAGS += ' -L' + `#{mecab_config} --libs-only-L`.chomp
+
 unless `#{mecab_config} --version`.chomp =~ /^0\.996/
   puts "[ERROR] Mecab 0.996 requires libmecab version 0.996."
   exit 1
